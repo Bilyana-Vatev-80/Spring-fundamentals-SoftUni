db0642fdd8a76eaf74a5da7c56eca59a978ae344
Bilyana Vatev <engineer_bilyana_vatev@email.com>
2021-10-01 11:10:27 +0100

    Initial commit:adding user controller

--- pathfinder/src/main/java/bg/softuni/pathfinder/repository/MessageRepository.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/repository/MessageRepository.java
@@ -1,8 +1,9 @@
 package bg.softuni.pathfinder.repository;
 
+import bg.softuni.pathfinder.modul.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageRepository,Long> {
+public interface MessageRepository extends JpaRepository<Message,Long> {
 }
